f85a505b4a20ec124e021579e57ef0d7fddf9f94
Wei-ying Chen <weiying.alex.chen@gmail.com>
2025-12-13 15:56:46 +0800

    Add Eruda

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (import.meta.env.DEV) {
+  import('eruda').then(({ default: eruda }) => eruda.init())
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />

adccebe780d25adb658b2e7495fc1314e9a81e82
Wei-ying Chen <weiying.alex.chen@gmail.com>
2026-01-21 21:05:27 +0800

    Update punctuation segment tests

--- tests/punctuationRuleSegments.test.ts
+++ tests/punctuationRuleSegments.test.ts
@@ -8,28 +8,20 @@ describe("punctuationRule (segments)", () => {
     const text = [
       "00:00:01:00\t00:00:02:00\tMarker",
       "Hello.",
-      "",
       "00:00:02:00\t00:00:03:00\tMarker",
       "this should be capitalized.",
-      "",
       "00:00:03:00\t00:00:04:00\tMarker",
       "This continues",
-      "",
       "00:00:04:00\t00:00:05:00\tMarker",
       "Next Starts Capital.",
-      "",
       "00:00:05:00\t00:00:06:00\tMarker",
       "He said",
-      "",
       "00:00:06:00\t00:00:07:00\tMarker",
       "\"Hello there.\"",
-      "",
       "00:00:07:00\t00:00:08:00\tMarker",
       "\"Unclosed.",
-      "",
       "00:00:08:00\t00:00:09:00\tMarker",
       "This line lacks terminal",
-      "",
     ].join("\n")
 
     const segments = parseSubs(text)
@@ -92,10 +84,8 @@ describe("punctuationRule (segments)", () => {
     const text = [
       "00:00:01:00\t00:00:02:00\tMarker",
       "\"it makes me feel like not knowing my way\"",
-      "",
       "00:00:02:00\t00:00:03:00\tMarker",
       "means there's something wrong with me.",
-      "",
     ].join("\n")
 
     const segments = parseSubs(text)
@@ -114,10 +104,8 @@ describe("punctuationRule (segments)", () => {
     const text = [
       "00:00:01:00\t00:00:02:00\tMarker",
       "\"it makes me feel like not knowing my way\"",
-      "",
       "00:00:02:00\t00:00:03:00\tMarker",
       "\"means there's something wrong with me.\"",
-      "",
     ].join("\n")
 
     const segments = parseSubs(text)

16351946163b109f345cbef475b5bc1b5646dc53
Wei-ying Chen <weiying.alex.chen@gmail.com>
2026-02-17 10:31:31 +0800

    Remove eruda mobile inspector

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-if (import.meta.env.DEV) {
-  import('eruda').then(({ default: eruda }) => eruda.init())
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />

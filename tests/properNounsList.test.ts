3a6fc08924b796e7b65f6344239ea13a60928f2f
Wei-ying Chen <weiying.alex.chen@gmail.com>
2026-09-17 17:15:54 +0800

    Add Sri Lanka proper noun

--- tests/properNounsList.test.ts
+++ tests/properNounsList.test.ts
@@ -27,6 +27,7 @@ describe("loadProperNouns", () => {
     expect(properNouns).toContain("Elitegroup Computer Systems")
     expect(properNouns).toContain("Tzu Chi")
     expect(properNouns).toContain("Tzu Cheng")
+    expect(properNouns).toContain("Sri Lanka")
     expect(properNouns).toContain("Nanjichang Borough Chief")
     expect(properNouns).toContain("Borough Chief Fang")
     expect(properNouns).toContain("Guanyin Bodhisattva")

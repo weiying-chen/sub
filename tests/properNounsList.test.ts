71320d956336b220694d0950b91c7cc80c5291d1
Wei-ying Chen <weiying.alex.chen@gmail.com>
2026-09-23 17:06:23 +0800

    Add Ecuador proper noun

--- tests/properNounsList.test.ts
+++ tests/properNounsList.test.ts
@@ -29,6 +29,7 @@ describe("loadProperNouns", () => {
     expect(properNouns).toContain("Tzu Cheng")
     expect(properNouns).toContain("Sri Lanka")
     expect(properNouns).toContain("Sabah")
+    expect(properNouns).toContain("Ecuador")
     expect(properNouns).toContain("Nanjichang Borough Chief")
     expect(properNouns).toContain("Borough Chief Fang")
     expect(properNouns).toContain("Guanyin Bodhisattva")
